2bb0ef0b36dd34ee6b9ede7f5a5d688d65819a91
therxmv <roma.kyzmuch@gmail.com>
2023-09-10 18:09:03 +0300

    Initial commit

---
+++ src/main/kotlin/com/therxmv/project8/Project8Application.kt
@@ -0,0 +1,12 @@
+package com.therxmv.project8
+
+import org.springframework.boot.Banner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Project8Application
+
+fun main(args: Array<String>) {
+	runApplication<Project8Application>(*args)
+}
---
+++ src/test/kotlin/com/therxmv/project8/Project8ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.therxmv.project8
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Project8ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
